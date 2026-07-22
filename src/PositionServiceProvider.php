0bd8021aca083795b0622e6cc5fbcce669a315e0
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-22 15:33:02 +0800

    Automated bulk repository update

---
+++ src/PositionServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rimba\Position;
+
+use Rimba\Base\BitesServiceProvider;
+
+
+class PositionServiceProvider extends BitesServiceProvider
+{
+
+    protected function bootPackage(): void
+    {
+        //
+    }
+    protected function registerPackage(): void
+    {
+        //
+    }
+
+}
