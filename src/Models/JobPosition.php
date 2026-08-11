9cc6cf68d21879776f05521cd83e1fe07205a38e
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-11 15:46:51 +0800

    Release v1.2.11

--- src/Models/JobPosition.php
+++ src/Models/JobPosition.php
@@ -8,10 +8,12 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Rimba\Agreement\Models\Agreement;
 use Rimba\Attributing\Traits\HasPersonAttributes;
 use Rimba\Organization\Models\OrgUnit;
+use Rimba\People\Models\Staff;
 use Rimba\People\Models\StaffPosition;
 
 #[Fillable([
@@ -48,9 +50,28 @@ class JobPosition extends Model
         return $this->hasMany(StaffPosition::class);
     }
 
+    public function staffs(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Staff::class,
+            'staff_positions'
+        );
+    }
+
     public function agreement(): BelongsTo
     {
         return $this->belongsTo(Agreement::class);
+    }
+
+    public function occupants()
+    {
+        return $this->staffs()
+            ->whereHas(
+                'staffPositions',
+                fn ($q) => $q
+                    ->where('job_position_id', $this->id)
+                    ->where('status', 'active')
+            );
     }
 
     public function orgUnit(): BelongsTo
